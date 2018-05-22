e63ccddfb8a911eeebe66c7081dd03b683ba6e9d
Doctorfox10 <doctorfox29@yahoo.co.uk>
2018-05-22 08:26:37 +0100

    Testing args

--- main.php
+++ main.php
@@ -2,9 +2,7 @@
 $json_url = "http://api.ebulksms.com:8080/sendsms.json";
 //$json_input = json_decode(file('php://input'), true);
 $json_input = file('php://input');
-print_r($_ENV);
-$allvars = getenv();
-print_r($_POST);print_r($allvars);exit;
+print_r($_ENV);print_r($argv);print_r($argc);exit;
 $flash = 0;
 $time = strftime('%l:%M %p', time());
 $max_usd_rate = $json_input['max_usd_rate'];
